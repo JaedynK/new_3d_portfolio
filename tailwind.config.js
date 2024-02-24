bd1cdf24f7d138dff43b6c37f1e79863a8e0446c
Jaedyn Keister <jaedyn.keister@gmail.com>
2024-02-24 16:58:58 -0600

    fix background

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,7 @@ module.exports = {
         xs: "450px",
       },
       backgroundImage: {
-        "hero-pattern": "url('/src/assets/goldbg.png')",
+        "hero-pattern": "url('/src/assets/goldbg.jpeg')",
       },
     },
   },
